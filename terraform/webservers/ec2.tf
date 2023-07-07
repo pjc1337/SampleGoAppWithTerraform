8d9498abea8e4b14ecf34645bc1c6eb5ac644f85
pjc1337 <138540659+pjc1337@users.noreply.github.com>
2023-07-07 07:57:27 -0400

    increase instance size

--- terraform/webservers/ec2.tf
+++ terraform/webservers/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   count                       = local.web_server_count
   ami                         = data.aws_ami.ubuntu.id
-  instance_type               = "t2.micro"
+  instance_type               = "t2.small"
   vpc_security_group_ids      = [aws_security_group.web.id]
   subnet_id                   = data.terraform_remote_state.platform.outputs.public_subnets[count.index]
   key_name                    = aws_key_pair.key.key_name
